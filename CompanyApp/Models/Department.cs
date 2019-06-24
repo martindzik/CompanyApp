d16411777642bd3d54df57a303c482f03c3d21b8
Martin Dzik <martindzik94@gmail.com>
2019-06-24 11:41:06 +0200

    Add project files.

---
+++ CompanyApp/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyApp.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProjectId { get; set; }
+        public int LeaderId { get; set; }
+    }
+}
---
+++ CompanyApp/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
